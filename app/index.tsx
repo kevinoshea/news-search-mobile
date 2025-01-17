28a397b83873f3fa4587f1293dec8e412e70addc
Kevin O'Shea <kevinoshea.mail@gmail.com>
2025-01-17 11:45:58 +1100

    Add project skeleton

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Sample app</Text>
+    </SafeAreaView>
+  );
+}
